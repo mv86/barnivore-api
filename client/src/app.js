8ca9c0d50c1cd20252d0893825a0c78077201a63
Max <mveasey87@gmail.com>
2016-12-26 20:23:26 +0000

    can now use the brewery select box to choose a company

--- client/src/app.js
+++ client/src/app.js
@@ -3,27 +3,27 @@ var countrySelect = require('./views/countrySelect.js');
 
 var app = function() {
   countrySelect.populateCountrySelect();
-  countrySelector = document.getElementById('country-select');
-  brewerySelector = document.getElementById('brewery-select');
-  alcoholTypeSelect = document.getElementById('type-select');
+  var countrySelector = document.getElementById('country-select');
+  var brewerySelector = document.getElementById('brewery-select');
+  var alcoholTypeSelect = document.getElementById('type-select');
+  var waitOption = document.getElementById('wait');
   alcoholTypeSelect.onchange = function() {
     countrySelector.style.visibility = 'hidden';
     brewerySelector.style.visibility = 'hidden';
-    waitOption = document.getElementById('wait');
     if (alcoholTypeSelect.value !== 'select') {
       waitOption.hidden = false;
       ajaxHelper.makeGetRequest(alcoholTypeSelect.value, apiInfo)
       alcoholTypeSelect.options.selectedIndex = 4;
+      waitOption.hidden = true;
     };
   };
   apiInfo = function(apiData) {
     alcoholTypeSelect.options.selectedIndex = 0;
-    waitOption.hidden = true;
     countrySelector.options.selectedIndex = 0;
-    breweries = JSON.parse(apiData)
+    var breweries = JSON.parse(apiData)
     countrySelector.style.visibility = 'visible';
     countrySelector.onchange = function() {
-      selectedCountryBreweries = [];
+      var selectedCountryBreweries = [];
       for (brewery of breweries) {
         if (this.value === brewery.company.country) {
           selectedCountryBreweries.push(brewery);
@@ -34,17 +34,29 @@ var app = function() {
   };
   populateBrewerySelect = function(breweries) {
     brewerySelector.style.visibility = 'visible';
+    console.log(breweries)
     brewerySelector.options.length = 1;
     if (breweries.length < 1) {
       brewerySelector.options[0].innerText = 
-      "Sorry None found" ;
+      "None Found" ;
     } else {
-      for (brewery of selectedCountryBreweries) {
-        brewerySelector.options[0].innerText = 
-        "Select Brewery" ;
+      brewerySelector.options[0].innerText = 
+      "Select Brewery" ;
+      for (brewery of breweries) {
         option = document.createElement('option');
         option.innerText = brewery.company.company_name;
         brewerySelector.appendChild(option);
+      }
+    }
+    brewerySelector.onchange = function() {
+      for (brewery of breweries) {
+        if (this.value === brewery.company.company_name) {
+          ul = document.getElementById('brewery-display')
+          ul.innerHTML = '';
+          liName = document.createElement('li')
+          liName.innerText = brewery.company.company_name;
+          ul.appendChild(liName);
+        }
       }
     }
   }
