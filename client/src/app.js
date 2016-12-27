d9a2f22b7ee2746e95d2b1a01a234bc86b479d07
Max <mveasey87@gmail.com>
2016-12-27 18:43:02 +0000

    edited selectPopulator to populate countrySelect box dynamically

--- client/src/app.js
+++ client/src/app.js
@@ -1,21 +1,8 @@
 var selectPopulator = require('./views/selectPopulator.js');
 
 var app = function() {
-  selectPopulator.populateCountrySelect();
-  selectPopulator.setupApiRequest();
+  // selectPopulator.populateCountrySelect();
+  selectPopulator();
 }
 
 window.onload = app;
-
-
-//  function compare(a, b) {
-//    if (a < b)
-//      return -1;
-//    if (a > b)
-//      return 1;
-//    return 0;
-//  }
-//  breweryCountries.sort(compare)
-//  filteredArray = breweryCountries.filter( function( item, index, inputArray ) {
-//   return inputArray.indexOf(item) == index;
-// });
--- client/src/views/selectPopulator.js
+++ client/src/views/selectPopulator.js
@@ -1,42 +1,14 @@
 var ajaxHelper = require('../helper/ajaxHelper.js');
 
-var countrySelect = {
- countries: ["Argentina", "Australia", "Austria",
- "Barbados", "Belgium", "Belize", "Bermuda", "Brazil",
- "Canada", "Cayman Islands", "Chile", "China", 
- "Columbia", "Costa Rica", "Croatia", "Cyprus", 
- "Czech Republic", "Denmark", "Dominican Republic", 
- "England", "Estonia", "Fiji", "Finland", "France", 
- "French Guinea", "Georgia", "Germany", "Greece", 
- "Guatemala", "Holland", "Hong Kong", "Hungary", 
- "Iceland", "India", "Ireland", "Isle of Man", "Israel", 
- "Italy", "Jamaica", "Japan", "Kenya", "Latvia", 
- "Lebanon", "Luxembourg", "Malta", "Maryland", "Mexico", 
- "Montenegro", "Namibia", "Netherlands", 
- "New Zealand", "Nicaragua", "Norway", "Palestine", 
- "Peru", "Philippines", "Poland", "Portugal", 
- "Puerto Rico", "Romania", "Russia", "Scotland", 
- "Singaport", "Slovakia", "Slovenia", "South Australia", 
- "South Africa", "Spain", "Sweden", "Switzerland", 
- "Tasmania", "Thailand", "The Netherlands", "Turkey", 
- "UK", "Ukraine", "USA", "United Kingdom", "Uruguay",
- "Wales", "Venezuela"],
- populateCountrySelect: function() {
-   var select = document.getElementById('country-select');
-   for (country of this.countries) {
-     option = document.createElement('option')
-     option.innerText = country;
-     select.appendChild(option);
-   };
- }, 
- setupApiRequest: function() {
+var setupApiRequests = function() {
   var countrySelector = document.getElementById('country-select');
-  var brewerySelector = document.getElementById('brewery-select');
+  var companySelector = document.getElementById('company-select');
   var alcoholTypeSelect = document.getElementById('type-select');
   var waitOption = document.getElementById('wait');
+
   alcoholTypeSelect.onchange = function() {
     countrySelector.style.visibility = 'hidden';
-    brewerySelector.style.visibility = 'hidden';
+    companySelector.style.visibility = 'hidden';
     if (alcoholTypeSelect.value !== 'select') {
       waitOption.hidden = false;
       ajaxHelper.makeGetRequest(alcoholTypeSelect.value, apiInfo)
@@ -45,50 +17,100 @@ var countrySelect = {
     };
   };
   var apiInfo = function(apiData) {
+    var companies = JSON.parse(apiData)
+    populateCountrySelect(companies);
     alcoholTypeSelect.options.selectedIndex = 0;
     countrySelector.options.selectedIndex = 0;
-    var breweries = JSON.parse(apiData)
     countrySelector.style.visibility = 'visible';
     countrySelector.onchange = function() {
-      var selectedCountryBreweries = [];
-      for (brewery of breweries) {
-        if (this.value === brewery.company.country) {
-          selectedCountryBreweries.push(brewery);
+      var selectedCountryCompanies = [];
+      for (company of companies) {
+        if (this.value === company.company.country) {
+          selectedCountryCompanies.push(company);
         };
       };
-      populateBrewerySelect(selectedCountryBreweries);
+      populateCompanySelect(selectedCountryCompanies);
     };
   };
-  var populateBrewerySelect = function(breweries) {
-    brewerySelector.style.visibility = 'visible';
-    ul = document.getElementById('brewery-display')
-    brewerySelector.options.length = 1;
-    if (breweries.length < 1) {
-      ul.innerHTML = '';
-      brewerySelector.options[0].innerText = 
-      "None Found" ;
-    } else {
-      ul.innerHTML = '';
-      brewerySelector.options[0].innerText = 
-      "Select Brewery" ;
-      for (brewery of breweries) {
-        option = document.createElement('option');
-        option.innerText = brewery.company.company_name;
-        brewerySelector.appendChild(option);
-      }
-    }
-    brewerySelector.onchange = function() {
-      for (brewery of breweries) {
-        if (this.value === brewery.company.company_name) {
-          ul.innerHTML = '';
-          liName = document.createElement('li')
-          liName.innerText = brewery.company.company_name;
-          ul.appendChild(liName);
-        }
-      }
-    }
+  var populateCountrySelect = function(companies) {
+    var countries = [];
+    for (company of companies) {
+      if (company.company.country !== "") {
+        countries.push(company.company.country);
+      };
+    };
+
+    var compare = function (a, b) {
+     if (a < b)
+       return -1;
+     if (a > b)
+       return 1;
+     return 0;
+   }
+   countries.sort(compare)
+
+   var filteredCountries = countries.filter( function( item, index, inputArray ) {
+    return inputArray.indexOf(item) == index;
+  });
+
+   for (country of filteredCountries) {
+    option = document.createElement('option')
+    option.innerText = country;
+    countrySelector.appendChild(option);
   };
-}
+};
+var populateCompanySelect = function(companies) {
+  companySelector.style.visibility = 'visible';
+  ul = document.getElementById('company-display')
+  companySelector.options.length = 1;
+  ul.innerHTML = '';
+
+  for (company of companies) {
+    option = document.createElement('option');
+    option.innerText = company.company.company_name;
+    companySelector.appendChild(option);
+  }
+  
+  companySelector.onchange = function() {
+    for (company of companies) {
+      if (this.value === company.company.company_name) {
+        ul.innerHTML = '';
+        liName = document.createElement('li')
+        liName.innerText = company.company.company_name;
+        ul.appendChild(liName);
+      }
+    }
+  }
+};
 };
 
-module.exports = countrySelect;
+module.exports = setupApiRequests;
+
+// countries: ["Argentina", "Australia", "Austria",
+// "Barbados", "Belgium", "Belize", "Bermuda", "Brazil",
+// "Canada", "Cayman Islands", "Chile", "China", 
+// "Columbia", "Costa Rica", "Croatia", "Cyprus", 
+// "Czech Republic", "Denmark", "Dominican Republic", 
+// "England", "Estonia", "Fiji", "Finland", "France", 
+// "French Guinea", "Georgia", "Germany", "Greece", 
+// "Guatemala", "Holland", "Hong Kong", "Hungary", 
+// "Iceland", "India", "Ireland", "Isle of Man", "Israel", 
+// "Italy", "Jamaica", "Japan", "Kenya", "Latvia", 
+// "Lebanon", "Luxembourg", "Malta", "Maryland", "Mexico", 
+// "Montenegro", "Namibia", "Netherlands", 
+// "New Zealand", "Nicaragua", "Norway", "Palestine", 
+// "Peru", "Philippines", "Poland", "Portugal", 
+// "Puerto Rico", "Romania", "Russia", "Scotland", 
+// "Singaport", "Slovakia", "Slovenia", "South Australia", 
+// "South Africa", "Spain", "Sweden", "Switzerland", 
+// "Tasmania", "Thailand", "The Netherlands", "Turkey", 
+// "UK", "Ukraine", "USA", "United Kingdom", "Uruguay",
+// "Wales", "Venezuela"],
+// populateCountrySelect: function() {
+//   var select = document.getElementById('country-select');
+//   for (country of this.countries) {
+//     option = document.createElement('option')
+//     option.innerText = country;
+//     select.appendChild(option);
+//   };
+// }, 
